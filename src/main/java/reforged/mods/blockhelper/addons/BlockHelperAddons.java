ba006875031219bbaa0f62714ce74d262ac5e2b1
CrossVas <cross.wanted.1.4@gmail.com>
2024-04-26 16:17:25 +0900

    version bump

--- src/main/java/reforged/mods/blockhelper/addons/BlockHelperAddons.java
+++ src/main/java/reforged/mods/blockhelper/addons/BlockHelperAddons.java
@@ -6,7 +6,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import reforged.mods.blockhelper.addons.proxy.CommonProxy;
 
-@Mod(modid = "BlockHelperAddons", name = "Block Helper Addons", version = "1.4.7-1.0.1", acceptedMinecraftVersions = "[1.4.7]", dependencies =
+@Mod(modid = "BlockHelperAddons", name = "Block Helper Addons", version = "1.4.7-1.0.2", acceptedMinecraftVersions = "[1.4.7]", dependencies =
         "required-after:IC2;required-after:mod_BlockHelper;after:AdvancedMachines;after:AdvancedPowerManagement;after:ChargePads;after:GregTech_Addon;after:AdvancedSolarPanel;after:AdvancedMachines;after:AdvancedPowerManagement")
 public class BlockHelperAddons {
 
