b4ffd1fc6e33aeec2597b3948f2e52c10a0c44f3
CrossVas <cross.wanted.1.4@gmail.com>
2024-12-14 20:42:25 +0900

    add defaultBarSize size check just in case

--- src/main/java/reforged/mods/blockhelper/addons/BlockHelperAddonsConfig.java
+++ src/main/java/reforged/mods/blockhelper/addons/BlockHelperAddonsConfig.java
@@ -1,6 +1,7 @@
 package reforged.mods.blockhelper.addons;
 
 import cpw.mods.fml.relauncher.FMLInjectionData;
+import mods.vintage.core.VintageCore;
 import mods.vintage.core.helpers.ConfigHelper;
 import net.minecraftforge.common.Configuration;
 
@@ -19,6 +20,10 @@ public class BlockHelperAddonsConfig {
 
         LANGS = ConfigHelper.getStrings(MAIN_CONFIG, "localization", "langs", new String[] {"en_US", "ru_RU"}, "Supported localizations.");
         DEFAULT_BAR_SIZE = ConfigHelper.getInt(MAIN_CONFIG, "general", "defaultBarSize", 25, Integer.MAX_VALUE, DEFAULT_BAR_SIZE, "Default Bar Element Size.\nSome localizations might require extra space (ru_RU for instance requires at least extra 10 size (35) to display the info).\nFeel free to change this to fit your language.");
+        if (DEFAULT_BAR_SIZE < 25) {
+            VintageCore.LOGGER.warning("defaultBarSize needs to be higher or equal to 25 for it to display info properly! Reverting to default (25)!");
+            DEFAULT_BAR_SIZE = ConfigHelper.getInt(MAIN_CONFIG, "general", "defaultBarSize", 25, Integer.MAX_VALUE, DEFAULT_BAR_SIZE, "Default Bar Element Size.\nSome localizations might require extra space (ru_RU for instance requires at least extra 10 size (35) to display the info).\nFeel free to change this to fit your language.");
+        }
 
         if (MAIN_CONFIG != null) {
             MAIN_CONFIG.save();
