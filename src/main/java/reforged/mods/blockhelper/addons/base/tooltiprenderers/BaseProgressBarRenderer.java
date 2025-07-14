e9c3396bca4213ff039651fcb25481856bd318e9
CrossVas <cross.wanted.1.4@gmail.com>
2025-07-14 20:34:46 +0300

    fixed: refactored ProgressBarElement This unfortunately makes my bars have hardcoded width due to breaking changes on BlockHelper side. Now there is a config for barMaxWidth for localizations that are longer than English. Sorry.

--- src/main/java/reforged/mods/blockhelper/addons/base/tooltiprenderers/BaseProgressBarRenderer.java
+++ src/main/java/reforged/mods/blockhelper/addons/base/tooltiprenderers/BaseProgressBarRenderer.java
@@ -3,67 +3,61 @@ package reforged.mods.blockhelper.addons.base.tooltiprenderers;
 import mcp.mobius.waila.api.ICommonAccessor;
 import mcp.mobius.waila.api.ITooltipRenderer;
 import mcp.mobius.waila.api.impl.TipList;
-import mcp.mobius.waila.mod_BlockHelper;
 import mcp.mobius.waila.overlay.RayTracing;
 import mcp.mobius.waila.overlay.Tooltip;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.Dimension;
-import org.lwjgl.util.Rectangle;
+import reforged.mods.blockhelper.addons.BlockHelperAddons;
 import reforged.mods.blockhelper.addons.utils.ColorUtils;
 import reforged.mods.blockhelper.addons.utils.GuiHelper;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BaseProgressBarRenderer implements ITooltipRenderer {
 
-    private final Map<String[], Tooltip> subTooltips = new HashMap<String[], Tooltip>();
-    private static Field FIELD_POS;
+    private final Map<List<String>, Tooltip> subTooltips = new HashMap<List<String>, Tooltip>();
+    private final Map<Tooltip, Integer> tooltipSize = new HashMap<Tooltip, Integer>();
 
-    static {
-        try {
-            FIELD_POS = Tooltip.class.getDeclaredField("pos");
-            FIELD_POS.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-    }
+    int offset = 4;
 
     @Override
     public Dimension getSize(String[] strings, ICommonAccessor accessor) {
-        if (FIELD_POS == null) {
+        if (strings == null || strings.length == 0) {
             return new Dimension();
         }
-        // An instance of our tooltip group for width consistency
-        Tooltip tooltip;
-        if (this.subTooltips.containsKey(strings)) {
-            // is present
-            tooltip = this.subTooltips.get(strings);
-        } else {
+
+        List<String> key = Arrays.asList(strings);
+        Tooltip tooltip = this.subTooltips.get(key);
+
+        if (tooltip == null) {
             TipList<String, String> list = new TipList<String, String>();
-            list.addAll(Arrays.asList(strings));
-            // create a new one
+            list.addAll(key);
             tooltip = new Tooltip(list, RayTracing.instance().getTargetStack());
-            this.subTooltips.put(strings, mod_BlockHelper.TICK_HANDLER.tooltip);
+            this.subTooltips.put(key, tooltip);
         }
 
-        int height = "1".equals(strings[4]) ? 10 : 11;
         FontRenderer font = Minecraft.getMinecraft().fontRenderer;
-        int maxLength = 0;
+
+        // Calculate max width of all lines
+        int maxLineWidth = 0;
         for (String str : strings) {
-            if (font.getStringWidth(str) > maxLength) {
-                maxLength = font.getStringWidth(str);
+            int width = font.getStringWidth(str);
+            if (width > maxLineWidth) {
+                maxLineWidth = width;
             }
         }
 
-        // safety check
-        if (tooltip == null) return new Dimension();
-        // get sizes for our tooltip
-        return new Dimension(maxLength, height);
+        tooltipSize.put(tooltip, maxLineWidth + offset);
+
+        boolean isStringOnly = strings.length > 4 && "1".equals(strings[4]);
+        int height = isStringOnly ? 10 : 11;
+
+        return new Dimension(Math.max(BlockHelperAddons.barMaxWidth, maxLineWidth + offset), height);
     }
 
     /**
@@ -78,38 +72,46 @@ public class BaseProgressBarRenderer implements ITooltipRenderer {
      * */
 
     @Override
-    public void draw(String[] strings, ICommonAccessor iWailaCommonAccessor, int x, int y) {
+    public void draw(String[] strings, ICommonAccessor accessor, int x, int y) {
+        if (strings == null || strings.length < 7) return;
 
-        Tooltip tooltip = subTooltips.get(strings);
-        if (tooltip != null) {
-            Rectangle rectangle = new Rectangle();
-            rectangle.setHeight(11);
-            try {
-                rectangle.setWidth(((Rectangle) FIELD_POS.get(tooltip)).getWidth() - 37);
-            } catch (IllegalAccessException exception) {
-                exception.printStackTrace();
-                return;
-            }
+        FontRenderer font = Minecraft.getMinecraft().fontRenderer;
 
-            GL11.glEnable(GL11.GL_BLEND);
-            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        int current = Integer.parseInt(strings[0]);
+        int max = Integer.parseInt(strings[1]);
+        int color = Integer.parseInt(strings[2]);
+        String text = strings[3];
+        boolean isStringOnly = "1".equals(strings[4]);
+        boolean centered = "1".equals(strings[5]);
+        String fluidStringId = strings[6];
 
-            int current = Integer.parseInt(strings[0]);
-            int max = Integer.parseInt(strings[1]);
-            int color = Integer.parseInt(strings[2]);
-            String text = strings[3];
-            boolean isStringOnly = "1".equals(strings[4]);
-            boolean centered = "1".equals(strings[5]);
-            String fluidStringId = strings[6];
-            FontRenderer font = Minecraft.getMinecraft().fontRenderer;
-            if (!isStringOnly) {
-                GuiHelper.INSTANCE.render(current, max, x, y, rectangle.getWidth(), rectangle.getHeight() + 1, color, fluidStringId);
-                x += rectangle.getWidth() / 2 - font.getStringWidth(text) / 2 + 1;
-            }
-            if (centered) {
-                x += rectangle.getWidth() / 2 - font.getStringWidth(text) / 2 + 1;
-            }
-            font.drawStringWithShadow(text, x, y + 2, ColorUtils.WHITE);
+        int maxLineWidth = 0;
+
+        Tooltip tooltips = this.subTooltips.get(Arrays.asList(strings));
+        if (tooltips != null) {
+            maxLineWidth = tooltipSize.get(tooltips);
         }
+
+        maxLineWidth = Math.max(BlockHelperAddons.barMaxWidth, maxLineWidth + offset);
+
+        int barHeight = 11;
+
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+        int textX = x;
+        if (!isStringOnly) {
+            // Draw the background bar using full width
+            GuiHelper.INSTANCE.render(current, max, x, y, maxLineWidth, barHeight + 1, color, fluidStringId);
+
+            // Center the text inside the full-width bar
+            textX += maxLineWidth / 2 - font.getStringWidth(text) / 2 + 1;
+        }
+
+        if (centered && isStringOnly) {
+            textX += maxLineWidth / 2 - font.getStringWidth(text) / 2 + 1;
+        }
+
+        font.drawStringWithShadow(text, textX, y + 2, ColorUtils.WHITE);
     }
 }
